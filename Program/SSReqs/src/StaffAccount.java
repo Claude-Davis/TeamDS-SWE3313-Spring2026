d531a2acc023898ab980ed6edd0e1aef59322435
StPatt <patrickswaney1@gmail.com>
2026-03-15 22:09:44 -0400

    Update StaffAccount.java
    Added more getters and setters from SS Requirements

--- Program/SSReqs/src/StaffAccount.java
+++ Program/SSReqs/src/StaffAccount.java
@@ -42,8 +42,130 @@ public class StaffAccount
         this.emailWork = emailWork;
     }
 
+    //getters
     public String getFullName() //returns full name of Staffer
     {
         return firstName + " " + middleName + " " + lastName;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getEmailPersonal()
+    {
+        return emailPersonal;
+    }
+
+    public String getEmailWork()
+    {
+        return emailWork;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public String getAddress() //returns full address of Staffer
+    {
+        return addressNumber + " " + streetName + " " + city + " " + state + " " + zipcode;
+    }
+
+    public LocalDate getBirthDate()
+    {
+        return birthDate;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    //setters
+    public void setFirstName(String firstName)
+    {
+         this.firstName = firstName;
+    }
+
+    public void setMiddleName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setRole(String role)
+    {
+        this.role = role;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public void setHourlyRate(Double hourlyRate)
+    {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public void setBirthDate(LocalDate birthDate)
+    {
+        this.birthDate = birthDate;
+    }
+
+    public void setHireDate(LocalDate hireDate)
+    {
+        this.hireDate = hireDate;
+    }
+
+    public void setAddressNumber(String addressNumber)
+    {
+        this.addressNumber = addressNumber;
+    }
+
+    public void setStreetName(String streetName)
+    {
+        this.streetName = streetName;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public void setZipcode(String zipcode)
+    {
+        this.zipcode = zipcode;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailPersonal(String emailPersonal)
+    {
+        this.emailPersonal = emailPersonal;
+    }
+
+    public void setEmailWork(String emailWork)
+    {
+        this.emailWork = emailWork;
     }
 }
